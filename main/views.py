07fa85cd6b8ef0ee960cc9057c4a6cbb74192779
Adiieeee <adiansyah.nugraha@gmail.com>
2024-09-09 15:06:56 +0700

    menambahkan gambar

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'name' : 'Phantom Mask',
+        'image' : 'https://imgur.com/a/U0fYFir',
         'price': '700000',
         'description': 'A mysterious Mask found in the depth of mountains, legend said that this mask once belong the the monkey king itself'
     }
