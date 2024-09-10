0305dc103675b74ca53be6e537081d2fd4090110
Adiieeee <adiansyah.nugraha@gmail.com>
2024-09-10 23:56:58 +0700

    menambahkan nama, kelas dan nama toko

--- main/views.py
+++ main/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
+        'toko' : 'Toko Izaka',
         'name' : 'Phantom Mask',
         'image' : 'https://imgur.com/a/U0fYFir',
         'price': '700000',
-        'description': 'A mysterious Mask found in the depth of mountains, legend said that this mask once belong the the monkey king itself'
+        'description': 'A mysterious Mask found in the depth of mountains, legend said that this mask once belong the the monkey king itself',
+        'nama_kamu' : 'Muhammad Adiansyah',
+        'kelas' : 'PBP D'
     }
 
     return render(request, "main.html", context)
