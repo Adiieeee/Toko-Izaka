909d0e6964471d1c5ca2411a3a2470eb3f03477f
Adiieeee <adiansyah.nugraha@gmail.com>
2024-09-18 10:15:34 +0700

    pws deploy

--- toko_izaka/settings.py
+++ toko_izaka/settings.py
@@ -125,3 +125,5 @@ MEDIA_ROOT = BASE_DIR / 'media'  # This is the directory where your media files 
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://muhammad-adiansyah-tokoizaka.pbp.cs.ui.ac.id/", "https://muhammad-adiansyah-tokoizaka.pbp.cs.ui.ac.id/"]
