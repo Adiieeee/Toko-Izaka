282c59d40ee6f33f99913ac80e28afbc87995b19
Adiieeee <adiansyah.nugraha@gmail.com>
2024-09-20 00:40:48 +0700

    tugas 4 kecuali readme done

--- toko_izaka/settings.py
+++ toko_izaka/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h3e%yzlm-j=^gn2r53+*q!&a=q&2w4h$^fy#as5-2xxo*55@)o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-adiansyah-tokoizaka.pbp.cs.ui.ac.id"]
 
