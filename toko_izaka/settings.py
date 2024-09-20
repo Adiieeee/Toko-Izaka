669a6c2e818d50fd8b8fb81f28c96327622079dc
Adiieeee <adiansyah.nugraha@gmail.com>
2024-09-20 10:12:47 +0700

    cobaa

--- toko_izaka/settings.py
+++ toko_izaka/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+ 
 MEDIA_URL = '/media/'  # This is the URL that will serve your media files
 MEDIA_ROOT = BASE_DIR / 'media'  # This is the directory where your media files will be stored
 
