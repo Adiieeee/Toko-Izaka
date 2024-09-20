77dffca9f5d64e9bd83d50e885d27b8d453d8260
Adiieeee <adiansyah.nugraha@gmail.com>
2024-09-20 10:07:21 +0700

    mencoba pws lagi

--- toko_izaka/settings.py
+++ toko_izaka/settings.py
@@ -128,4 +128,4 @@ MEDIA_ROOT = BASE_DIR / 'media'  # This is the directory where your media files 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://muhammad-adiansyah-tokoizaka.pbp.cs.ui.ac.id", "https://muhammad-adiansyah-tokoizaka.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://muhammad-adiansyah-tokoizaka.pbp.cs.ui.ac.id/", "https://muhammad-adiansyah-tokoizaka.pbp.cs.ui.ac.id/"]
