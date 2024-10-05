016eb238c94689a3a19dae5fa14c9091e1bec1d7
Adiieeee <adiansyah.nugraha@gmail.com>
2024-10-05 12:48:42 +0700

    nambahin dari tugas 5

--- main/models.py
+++ main/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    price = models.IntegerField()
+    price = models.FloatField()
     description = models.TextField()
     image = models.ImageField()
 
